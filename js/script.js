3bdffadb5910a17bce1b9fa00335693e7eee09c9
Danila Sokolov <matsubus@mail.ru>
2020-12-09 12:24:20 +0300

    fix(AuthUser): fix storage Init

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ authButton.addEventListener('click', () => {
 
     let storageUsers = JSON.parse(localStorage.getItem('Users'));
 
-    if (storageUsers.length == 0) {
+    if (!storageUsers) {
         alert('Пользователь не найден!');
         return;
     }
